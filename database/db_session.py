ce0bc3e4a2415219fb26bce7818873d99912211a
Kirill I. A <ghztrikz1337@gmail.com>
2022-04-10 01:41:05 +0300

    Trying to host DB locally, connect to it, and initialize tables

--- database/db_session.py
+++ database/db_session.py
@@ -1,3 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import QueuePool
+from sqlalchemy import create_engine
+
 
 Base = declarative_base()
+
+path = 'database/db'
+engine = create_engine(url='postgresql+psycopg2://@localhost/' + path,
+                       echo=False, pool_size=10, max_overflow=0, poolclass=QueuePool, pool_pre_ping=True)
+
+print('Подключение к базе данных')
+session = sessionmaker(bind=engine)
+
+print('Инициализация таблиц')
+Base.metadata.create_all(engine)
