ffbbc29f74e96878163c10afd086acda8c02dbe8
Kirill I. A <ghztrikz1337@gmail.com>
2022-04-11 01:22:43 +0300

    git.ignore update

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@ import re
 
 EMAIL_PATTERN = re.compile('^[A-Za-z0-9.+_-]+@[A-Za-z0-9._-]+\.[a-zA-Z]*$')
 
+
 def email_is_valid(email: str) -> bool:
     if EMAIL_PATTERN.match(email):
         return True
