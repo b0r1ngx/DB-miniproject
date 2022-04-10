653eeaadd4caf388d0d000860b8e0b5953bd0c6d
Kirill I. A <ghztrikz1337@gmail.com>
2022-04-11 01:17:27 +0300

    email_is_valid() Guru+ implementation

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 import re
 
-EMAIL_PATTERN = re.compile('[^@]+@[^@]+\.[^@]+')
+EMAIL_PATTERN = re.compile('^[A-Za-z0-9.+_-]+@[A-Za-z0-9._-]+\.[a-zA-Z]*$')
 
 
 def email_is_valid(email: str) -> bool:
