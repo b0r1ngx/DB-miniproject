53daf2038b8dbae02d538515db0d5262a1a362dc
Kirill I. A <ghztrikz1337@gmail.com>
2022-04-11 01:13:56 +0300

    email_is_valid() implementation

--- helpers.py
+++ helpers.py
@@ -1,2 +1,9 @@
-def email_is_valid():
-    pass
+import re
+
+EMAIL_PATTERN = re.compile('[^@]+@[^@]+\.[^@]+')
+
+
+def email_is_valid(email: str) -> bool:
+    if EMAIL_PATTERN.match(email):
+        return True
+    return False
