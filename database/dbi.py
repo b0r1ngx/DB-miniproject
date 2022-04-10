b1618d3afa945f4a1318026609118a7e7bb239f8
TsaplinIA <tsapli.ia@edu.spbstu.ru>
2022-04-11 01:50:54 +0300

    check_user_exist is_admin

--- database/dbi.py
+++ database/dbi.py
@@ -6,12 +6,12 @@ from psycopg2.errors import *
 import re
 
 
-def insert_into_users(full_name, email, password, is_admin=False):
-    with Session() as s:
-        # "INSERT INTO users VALUES ({full_name}, {email}, {password}, {is_admin}, {created_at});
-        stmt = (insert(users).values(full_name, email, password, is_admin))
-        s.execute(stmt)
-        s.commit()
+# def insert_into_users(full_name, email, password, is_admin=False):
+#     with Session() as s:
+#         # "INSERT INTO users VALUES ({full_name}, {email}, {password}, {is_admin}, {created_at});
+#         stmt = (insert(users).values(full_name, email, password, is_admin))
+#         s.execute(stmt)
+#         s.commit()
 
 
 def login(email: str, password: str) -> int:
@@ -33,7 +33,7 @@ def login(email: str, password: str) -> int:
         return id[0]
 
 
-def get_user_id(email:str) -> int:
+def get_user_id(email: str) -> int:
     """
     Also can be used on registration() when need to check if user exists
     :param email:
@@ -81,8 +81,23 @@ def do_user_have_access_to_other_user_photos(owner: int, viewer: int) -> dict:
             }
     """
 
-
     pass
 
 
+def check_user_exist(user_id) -> bool:
+    """
 
+    :param user_id:
+    :return True: - User exist
+    :return False: - User not exist
+    """
+    pass  # TODO
+
+
+def is_admin(user_id) -> bool:
+    """
+
+    :param user_id:
+    :return True: - User is admin
+    :return False: - User isn't admin
+    """
